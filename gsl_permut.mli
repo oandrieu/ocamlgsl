67b9a5442b2385900e8de674735911bb7dc08414
Olivier Andrieu <oandrieu@gmail.com>
2009-03-09 01:39:03 +0000

    Gsl_permut: add the "cyclic form" functions

--- gsl_permut.mli
+++ gsl_permut.mli
@@ -30,9 +30,21 @@ external permute : permut -> 'a array -> unit
     = "ml_gsl_permute"
 external permute_barr : permut -> ('a, 'b, 'c) Bigarray.Array1.t -> unit
     = "ml_gsl_permute_barr"
+external permute_complex : permut -> Gsl_complex.complex_array -> unit
+    = "ml_gsl_permute_complex"
 
 external permute_inverse : permut -> 'a array -> unit
     = "ml_gsl_permute_inverse"
 external permute_inverse_barr : permut -> 
   ('a, 'b, 'c) Bigarray.Array1.t -> unit
     = "ml_gsl_permute_inverse_barr"
+external permute_inverse_complex : permut -> Gsl_complex.complex_array -> unit
+    = "ml_gsl_permute_inverse_complex"
+
+val mul : permut -> permut -> permut
+
+val linear_to_canonical : permut -> permut
+val canonical_to_linear : permut -> permut
+external inversions : permut -> int = "ml_gsl_permute_inversions"
+external canonical_cycles : permut -> int = "ml_gsl_permute_canonical_cycles"
+external linear_cycles : permut -> int = "ml_gsl_permute_linear_cycles"
